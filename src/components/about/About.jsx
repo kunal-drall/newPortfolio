e026cb8e7bdae1d3abcc83210b4cc2cd1b5c4e47
Kunal <kunaldrall29@gmail.com>
2025-04-05 16:58:18 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -19,14 +19,14 @@ const About = () => {
 
 
         <p className="about__description">
-       👋 Meet @Kunal - a tech enthusiast with a passion for cutting-edge technology and programming languages. He's interested in Solana, Rust, Solidity, Move, Web3, ReactJS,VUE, Typescript, Redux, NextJS, NodeJS, BlockChain technology, SQL, JAVA and Spring Boot making him a versatile and curious learner. Kunal is also interested in architecture like AWS and Azure.</br>
-
-🏍️ When he's not diving into the latest tech developments, Kunal indulges in his hobbies of 🎾 Lawn Tennis, 🥊 Muay Thai, and 🏍️ PitBikes, showcasing his love for speed and adventure.</br>
-
-🌱 With his extensive background and dedication, Kunal is a web3 founder and Full stack/blockchain developer.
-
-Overall, Kunal is a driven and dynamic individual who is passionate about the tech industry and living life to the fullest. Want to connect with him? Reach out to him via his preferred contact method. 📫 kunaldrall.at.gmail.com
-        </p>
+    👋 Meet @Kunal - a tech enthusiast with a passion for cutting-edge technology and programming languages. He's interested in Solana, Rust, Solidity, Move, Web3, ReactJS, VUE, Typescript, Redux, NextJS, NodeJS, BlockChain technology, SQL, JAVA and Spring Boot making him a versatile and curious learner. Kunal is also interested in architecture like AWS and Azure.<br />
+    <br />
+    🏍️ When he's not diving into the latest tech developments, Kunal indulges in his hobbies of 🎾 Lawn Tennis, 🥊 Muay Thai, and 🏍️ PitBikes, showcasing his love for speed and adventure.<br />
+    <br />
+    🌱 With his extensive background and dedication, Kunal is a web3 founder and Full stack/blockchain developer.<br />
+    <br />
+    Overall, Kunal is a driven and dynamic individual who is passionate about the tech industry and living life to the fullest. Want to connect with him? Reach out to him via his preferred contact method. 📫 kunaldrall.at.gmail.com
+</p>
 
         <a download="" href={CV} className="button button--flex">
           Download CV
